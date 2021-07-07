db5376ea5894f9d77e6bffe7d7734641ae1b78ff
reginaldj123 <gunt7701@gmail.com>
2021-07-07 16:46:05 +0000

    adding to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
+const choicesectionButtons = document.querySelectorAll('[data-selection]');
+choicesectionButtons.forEach(choicesectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const choicesName = selectionButton.dataset.selection 
+        makeSelection(selectionName)
+
+    });
+}
+    );
+    function makechoices(choices)
 console.log("conected");
