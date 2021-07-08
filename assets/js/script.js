d0bfc0794e439115c331d8a84aa585e20fd84a30
reginaldj123 <gunt7701@gmail.com>
2021-07-08 00:18:52 +0000

    http.server

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
 const choicesectionButtons = document.querySelectorAll('[data-selection]');
+const choices = [
+    {
+        name:Rock;
+        emoji:✊🏼;
+        beats:Scissors;
+    }
+    {
+        name:Paper;
+        emoji:✋🏻;
+        beats:Rock;
+    } {
+        name:Scissors;
+        emoji:✌🏼;
+        beats:Paper;
+    }
+]
 choicesectionButtons.forEach(choicesectionButton => {
     selectionButton.addEventListener('click',  e => {
         const choicesName = selectionButton.dataset.selection 
