5956255638a07cbb962124b0e7826617b7e4b6fe
reginaldj123 <gunt7701@gmail.com>
2021-07-10 15:54:12 +0000

    anding promt and rules box

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,6 +60,31 @@ function randomSelection() {
   const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
   return SELECTIONS[randomIndex]
 }
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 // on page load collects user name and show rules
 
 document.addEventListener("DOMContentLoaded", function(){
